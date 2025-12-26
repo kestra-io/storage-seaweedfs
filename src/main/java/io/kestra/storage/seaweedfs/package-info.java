771b363e49b79f647f67d948b6ba75720492dc1e
bishalbera <bishalbera2@gmail.com>
2025-12-26 16:40:50 +0530

    feat: introduce seaweedfs storage

---
+++ src/main/java/io/kestra/storage/seaweedfs/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "SeaweedFS",
+    description = "This sub-group of plugins contains tasks for interacting with SeaweedFS storage.\n",
+    categories = { PluginSubGroup.PluginCategory.STORAGE }
+)
+package io.kestra.storage.seaweedfs;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
