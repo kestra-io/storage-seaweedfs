993f395c6ce3979b5fa3fe931037e0160e54d67d
bishalbera <bishalbera2@gmail.com>
2026-01-03 08:02:53 +0530

    fix: fixed test container setup

--- src/test/java/io/kestra/storage/seaweedfs/SeaweedFSTestContainers.java
+++ src/test/java/io/kestra/storage/seaweedfs/SeaweedFSTestContainers.java
@@ -1,10 +1,10 @@
 package io.kestra.storage.seaweedfs;
 
 import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
@@ -12,125 +12,86 @@ import java.time.Duration;
 
 public class SeaweedFSTestContainers {
 
-    private static final DockerImageName SEAWEEDFS_IMAGE = DockerImageName.parse("chrislusf/seaweedfs:latest");
-    private static final Network NETWORK = Network.newNetwork();
+    private static final DockerImageName SEAWEEDFS_IMAGE =
+        DockerImageName.parse("chrislusf/seaweedfs:latest");
 
-    private static GenericContainer<?> masterContainer;
-    private static GenericContainer<?> volumeContainer;
-    private static GenericContainer<?> filerContainer;
+    private static GenericContainer<?> mainContainer;
 
-    public static final int MASTER_PORT = 9333;
-    public static final int VOLUME_PORT = 18080;  // Volume port (grPC will be 18080 + 10000 = 28080)
-    public static final int FILER_HTTP_PORT = 8888;
-    public static final int FILER_GRPC_PORT = 18888;
+    public static final int MASTER_PORT = 19333;
+    public static final int VOLUME_PORT = 18080;
+    public static final int FILER_HTTP_PORT = 18888;
+    public static final int FILER_GRPC_PORT = 19888;
 
     public static synchronized void start() {
-        if (masterContainer != null && masterContainer.isRunning()) {
-            System.out.println("SeaweedFS containers already running");
+        if (mainContainer != null && mainContainer.isRunning()) {
             return;
         }
 
         try {
-            System.out.println("Starting SeaweedFS TestContainers...");
+            System.out.println("Starting SeaweedFS TestContainer (Fixed Ports)...");
 
-            System.out.println("Starting master container...");
-            masterContainer = new GenericContainer<>(SEAWEEDFS_IMAGE)
-                .withNetwork(NETWORK)
-                .withNetworkAliases("seaweedfs-master")
-                .withCommand("master", "-ip=seaweedfs-master", "-port=" + MASTER_PORT)
-                .withExposedPorts(MASTER_PORT)
-                .waitingFor(Wait.forHttp("/cluster/status")
-                    .forPort(MASTER_PORT)
-                    .withStartupTimeout(Duration.ofSeconds(30)));
-
-            masterContainer.start();
-            System.out.println("Master container started on port: " + masterContainer.getMappedPort(MASTER_PORT));
-
-            // Start Volume Server with fixed port and publicUrl pointing to localhost
-            System.out.println("Starting volume container...");
-            volumeContainer = new GenericContainer<>(SEAWEEDFS_IMAGE)
-                .withNetwork(NETWORK)
-                .withNetworkAliases("seaweedfs-volume")
+            mainContainer = new GenericContainer<>(SEAWEEDFS_IMAGE)
+                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+                    new HostConfig().withPortBindings(
+                        // Map Host Port -> Container Port 1:1
+                        new PortBinding(Ports.Binding.bindPort(MASTER_PORT), new ExposedPort(MASTER_PORT)),
+                        new PortBinding(Ports.Binding.bindPort(VOLUME_PORT), new ExposedPort(VOLUME_PORT)),
+                        new PortBinding(Ports.Binding.bindPort(FILER_HTTP_PORT), new ExposedPort(FILER_HTTP_PORT)),
+                        new PortBinding(Ports.Binding.bindPort(FILER_GRPC_PORT), new ExposedPort(FILER_GRPC_PORT))
+                    )
+                ))
                 .withCommand(
-                    "volume",
-                    "-mserver=seaweedfs-master:" + MASTER_PORT,
-                    "-port=" + VOLUME_PORT,
-                    "-ip=seaweedfs-volume",
-                    "-publicUrl=localhost:" + VOLUME_PORT,
-                    "-max=100"
+                    "server",
+                    // advertise "localhost" so the Java Client (on host) can resolve it
+                    "-ip=localhost",
+                    // bind to 0.0.0.0 so the Docker Gateway can bridge the connection
+                    "-ip.bind=0.0.0.0",
+                    "-dir=/data",
+                    "-volume.max=100",
+                    "-master.port=" + MASTER_PORT,
+                    "-volume.port=" + VOLUME_PORT,
+                    "-filer",
+                    "-filer.port=" + FILER_HTTP_PORT,
+                    "-filer.port.grpc=" + FILER_GRPC_PORT
                 )
-                .withCreateContainerCmdModifier(cmd -> {
-                    cmd.getHostConfig().withPortBindings(
-                        new PortBinding(
-                            Ports.Binding.bindPort(VOLUME_PORT),
-                            new ExposedPort(VOLUME_PORT)
-                        )
-                    );
-                })
-                .dependsOn(masterContainer)
-                .waitingFor(Wait.forHttp("/status")
-                    .forPort(VOLUME_PORT)
-                    .withStartupTimeout(Duration.ofSeconds(30)));
+                .waitingFor(
+                    Wait.forHttp("/?pretty=y")
+                        .forPort(FILER_HTTP_PORT)
+                        .withStartupTimeout(Duration.ofSeconds(60))
+                );
 
-            volumeContainer.start();
-            System.out.println("Volume container started with publicUrl=localhost:" + VOLUME_PORT);
+            mainContainer.start();
 
-            System.out.println("Starting filer container...");
-            filerContainer = new GenericContainer<>(SEAWEEDFS_IMAGE)
-                .withNetwork(NETWORK)
-                .withNetworkAliases("seaweedfs-filer")
-                .withCommand(
-                    "filer",
-                    "-master=seaweedfs-master:" + MASTER_PORT,
-                    "-port=" + FILER_HTTP_PORT,
-                    "-port.grpc=" + FILER_GRPC_PORT,
-                    "-ip=seaweedfs-filer"
-                )
-                .withExposedPorts(FILER_HTTP_PORT, FILER_GRPC_PORT)
-                .dependsOn(volumeContainer)
-                .waitingFor(Wait.forHttp("/")
-                    .forPort(FILER_HTTP_PORT)
-                    .withStartupTimeout(Duration.ofSeconds(30)));
-
-            filerContainer.start();
-            System.out.println("Filer container started:");
-            System.out.println("  HTTP port: " + filerContainer.getMappedPort(FILER_HTTP_PORT));
-            System.out.println("  gRPC port: " + filerContainer.getMappedPort(FILER_GRPC_PORT));
-            System.out.println("  Host: " + filerContainer.getHost());
-
-            // Wait a bit for the cluster to stabilize
-            Thread.sleep(2000);
-            System.out.println("SeaweedFS TestContainers started successfully!");
+            System.out.println("SeaweedFS started!");
+            System.out.println("Filer HTTP: " + getFilerUrl());
 
         } catch (Exception e) {
-            System.err.println("ERROR: Failed to start SeaweedFS containers: " + e.getMessage());
-            e.printStackTrace();
-            throw new RuntimeException("Failed to start SeaweedFS TestContainers", e);
+            stop();
+            throw new RuntimeException("Failed to start SeaweedFS", e);
         }
     }
 
     public static synchronized void stop() {
-        if (filerContainer != null) {
-            filerContainer.stop();
+        if (mainContainer != null) {
+            mainContainer.stop();
+            mainContainer = null;
         }
-        if (volumeContainer != null) {
-            volumeContainer.stop();
-        }
-        if (masterContainer != null) {
-            masterContainer.stop();
-        }
-        NETWORK.close();
     }
 
     public static String getFilerHost() {
-        return filerContainer != null ? filerContainer.getHost() : "localhost";
+        return mainContainer.getHost();
     }
 
     public static int getFilerGrpcPort() {
-        return filerContainer != null ? filerContainer.getMappedPort(FILER_GRPC_PORT) : FILER_GRPC_PORT;
+        // Since we mapped 1:1, this is just FILER_GRPC_PORT
+        return FILER_GRPC_PORT;
     }
 
-    public static int getVolumePort() {
-        return VOLUME_PORT;
+    public static int getFilerHttpPort() {
+        return FILER_HTTP_PORT;
+    }
+
+    public static String getFilerUrl() {
+        return String.format("http://%s:%d", getFilerHost(), getFilerHttpPort());
     }
 }
